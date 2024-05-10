d2530812b62dfa0e9e8f9ab3ecc482442d8ba19e
Mykhailo Tokmakov <misha.tokmak@gmail.com>
2024-05-10 15:53:35 +0300

    Initial Commit

---
+++ SwiftfulThinkingContiniuedLeraning/SwiftfulThinkingContiniuedLeraningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingContiniuedLeraningApp.swift
+//  SwiftfulThinkingContiniuedLeraning
+//
+//  Created by Michael on 10.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingContiniuedLeraningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
