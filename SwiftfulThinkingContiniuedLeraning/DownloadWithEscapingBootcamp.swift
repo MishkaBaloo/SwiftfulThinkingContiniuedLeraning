595519ee0a72868f1b5d07720cf24fe38562b0b9
Mykhailo Tokmakov <misha.tokmak@gmail.com>
2024-05-16 21:20:53 +0300

    DownloadWithEscaping Bootcamp

---
+++ SwiftfulThinkingContiniuedLeraning/DownloadWithEscapingBootcamp.swift
@@ -0,0 +1,80 @@
+//
+//  DownloadWithEscapingBootcamp.swift
+//  SwiftfulThinkingContiniuedLeraning
+//
+//  Created by Michael on 16.05.2024.
+//
+
+import SwiftUI
+
+struct PostModel: Identifiable, Codable {
+    let userId: Int
+    let id: Int
+    let title: String
+    let body: String
+}
+
+class DownloadWithEscapingViewModel: ObservableObject {
+    
+    @Published var posts: [PostModel] = []
+    
+    init() {
+        getPost()
+    }
+    
+    func getPost() {
+        
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        
+        downloadData(formURL: url) { returnedData in
+            if let data = returnedData {
+                guard let newPosts = try? JSONDecoder().decode([PostModel].self, from: data) else { return }
+                DispatchQueue.main.async { [weak self] in
+                    self?.posts = newPosts
+                }
+            } else {
+                print("No data returned.")
+            }
+        }
+    }
+    
+    func downloadData(formURL url: URL, comletionHandler: @escaping (_ data: Data?) -> ()) {
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard
+                let data = data,
+                error == nil,
+                let response = response as? HTTPURLResponse,
+                response.statusCode >= 200 && response.statusCode < 300 else {
+                print("Error downloading data.")
+                comletionHandler(nil)
+                return
+            }
+            
+            comletionHandler(data)
+            
+        }.resume() // it's start of function
+    }
+}
+
+struct DownloadWithEscapingBootcamp: View {
+    
+    @StateObject var vm = DownloadWithEscapingViewModel()
+    
+    var body: some View {
+        List {
+            ForEach(vm.posts) { post in
+                VStack(alignment: .leading) {
+                    Text(post.title)
+                        .font(.headline)
+                    Text(post.body)
+                        .foregroundStyle(Color.gray)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+            }
+        }
+    }
+}
+
+#Preview {
+    DownloadWithEscapingBootcamp()
+}
--- SwiftfulThinkingContiniuedLeraning/SwiftfulThinkingContiniuedLeraningApp.swift
+++ SwiftfulThinkingContiniuedLeraning/SwiftfulThinkingContiniuedLeraningApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftfulThinkingContiniuedLeraningApp: App {
     var body: some Scene {
         WindowGroup {
-            CodableBootcamp()
+            DownloadWithEscapingBootcamp()
         }
     }
 }
